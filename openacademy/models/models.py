f51112c3aee92ad9903c58a22ffff07b8bee188a
Keyla Aviles <kaviles@alerta.com.mx>
2020-08-25 11:01:21 -0600

    Tema 5 parte 2

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import time
 
 
 class Course(models.Model):
@@ -20,11 +21,18 @@ class Session(models.Model):
 
     name=fields.Char(required=True)
     start_date=fields.Date()
+    #datetime_test=fields.Datetime(default=time.strftime('%Y-%m-%d %H:%M:%S'))
     duration=fields.Float(digits=(6,2),help="Duration in days")
     seats=fields.Integer(string="Number of seats")
     instructor_id=fields.Many2one('res.partner','Instructor',domain=['|',('instructor','=','True'),('category_id.name','ilike','Teacher')])
     course_id=fields.Many2one('openacademy.course',ondelete='cascade',
                                 string="Course", required=True)
     attendee_ids=fields.Many2many('res.partner',string="Attendees")
-    
-   
+    taken_seats=fields.Float(compute='_taken_seats')
+
+    @api.depends('seats','attendee_ids')
+    def _taken_seats(self):
+        import pdb; pdb.set_trace()
+        #for record in self.filtered(lambda r: r.seats != 0):
+        for record in self.filtered(lambda r: r.seats):
+            record.taken_seats= 100.0 * len(record.attendee_ids) / record.seats
