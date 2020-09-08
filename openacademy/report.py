07a301ff774bdcef6c669fd2979d25495cf902f6
Keyla Aviles <kaviles@alerta.com.mx>
2020-09-07 19:31:22 -0600

    [REF] openacademy: Add board

--- openacademy/report.py
+++ openacademy/report.py
@@ -1,19 +1,28 @@
+  
 # -*- coding: utf-8 -*-
+# Copyright <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
 from openerp import api,models
 
 class ReportSession(models.AbstractModel):
     _name="report.openacademy.report_session_view"
-
     
-    def render_html(self,docids,data=None):
-        report_obj = self.env["report"]
-        report = report_obj._get_report_from_name("openacademy.report_session")
-        docargs = {
-            "docs_ids": docids,
-            "doc_model": report.model,
-            "docs": self.env['openacademy.session'].browse(docids),
-            "other_variable": 'other value'
+    def _get_report_values(self,docids,data=None):
+        # report_obj = self.env["report"]
+        # report = report_obj._get_report_from_name("openacademy.report_session")
+        # docargs = {
+        #     "docs_ids": docids,
+        #     "doc_model": report.model,
+        #     "docs": self.env['openacademy.session'].browse(docids),
+        #     "other_variable": 'other value'
+        # }
+        # return report_obj.render("openacademy.report_session_view",docargs)
+        docs = self.env['openacademy.session'].browse(docids)
+        return {
+            'doc_ids': docs.ids,
+            'doc_model': 'openacademy.session',
+            'docs': docs,
+            'other_variable': 'other value',
         }
-        return report_obj.render("openacademy.report_session_view",docargs)
