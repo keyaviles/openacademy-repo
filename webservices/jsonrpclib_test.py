b0943e7834ec581dd8ecd9ac1f3260a03c2012a1
Keyla Aviles <kaviles@alerta.com.mx>
2020-09-10 21:29:30 -0600

    [ADD] webservices calls

---
+++ webservices/jsonrpclib_test.py
@@ -0,0 +1,38 @@
+import json
+import random
+import urllib.request
+
+HOST = 'localhost'
+PORT = 8069
+DB = 'odoodb'
+USER = 'admin'
+PASS = 'admin'
+
+def json_rpc(url, method, params):
+    data = {
+        "jsonrpc": "2.0",
+        "method": method,
+        "params": params,
+        "id": random.randint(0, 1000000000),
+    }
+    req = urllib.request.Request(url=url, data=json.dumps(data).encode(), headers={
+        "Content-Type":"application/json",
+    })
+    reply = json.loads(urllib.request.urlopen(req).read().decode('UTF8'))
+    if reply.get("error"):
+        raise Exception(reply["error"])
+    return reply["result"]
+
+def call(url, service, method, *args):
+    return json_rpc(url, "call", {"service": service, "method": method, "args": args})
+
+# log in the given database
+url = "http://%s:%s/jsonrpc" % (HOST, PORT)
+uid = call(url, "common", "login", DB, USER, PASS)
+
+# create a new note
+args = {
+    'name' : 'Python Masterclass',
+
+}
+note_id = call(url, "object", "execute", DB, uid, PASS, 'openacademy.course', 'create', args)
