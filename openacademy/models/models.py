5056afaae5a360af4792c51f776699805703e2d4
Keyla Aviles <kaviles@alerta.com.mx>
2020-09-24 20:26:10 -0600

    [REF] openacademy: Fix pylints fails

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api, exceptions, _
+from datetime import timedelta
 import time
 from psycopg2 import IntegrityError
-from datetime import timedelta
+from odoo import models, fields, api, exceptions, _
 
-def get_uid(self,*a):
+
+def get_uid(self, *a):
     # import pdb; pdb.set_trace()
     return self.env.uid
 
@@ -14,17 +15,17 @@ class Course(models.Model):
     _name = 'openacademy.course'
     _description = 'Clase o modelo para definir cursos'
 
-    name = fields.Char(string="Title",required=True)
-    description=fields.Text()
-    responsible_id=fields.Many2one(
-        'res.users',string="Responsible",
-        index=True,ondelete='set null',default=get_uid)
-    sessions_ids=fields.One2many('openacademy.session','course_id')
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+    responsible_id = fields.Many2one(
+        'res.users', string="Responsible",
+        index=True, ondelete='set null', default=get_uid)
+    sessions_ids = fields.One2many('openacademy.session', 'course_id')
 
-    _sql_constraints=[
+    _sql_constraints = [
         ('name_description_check',
-        'CHECK( name != description )',
-        "The title of the course should not be the description"
+         'CHECK( name != description )',
+         "The title of the course should not be the description"
         ),
         ('name_unique',
          'UNIQUE(name)',
@@ -32,16 +33,16 @@ class Course(models.Model):
         ),
     ]
 
-    def copy(self,default=None):
+    def copy(self, default=None):
         if default is None:
             default = {}
-        copied_count=self.search_count([
-            ('name','ilike','Copy of %s%%' % (self.name))])
+        copied_count = self.search_count([
+            ('name', 'ilike', 'Copy of %s%%' % (self.name))])
         if not copied_count:
-            new_name=_("Copy of %s") % (self.name)
+            new_name = _("Copy of %s") % (self.name)
         else:
-            new_name=_("Copy of %s (%s)") % (self.name,copied_count)
-        default['name']=new_name
+            new_name = _("Copy of %s (%s)") % (self.name, copied_count)
+        default['name'] = new_name
         # try:
         return super(Course, self).copy(default)
         # except IntegrityError:
@@ -49,23 +50,25 @@ class Course(models.Model):
 
 class Session(models.Model):
     _name = 'openacademy.session'
-    _description='Clase o modelo para definir sesiones'
+    _description = 'Clase o modelo para definir sesiones'
 
-    name=fields.Char(required=True)
-    start_date=fields.Date(default=fields.Date.today)
-    datetime_test=fields.Datetime(default=fields.Datetime.now)
-    duration=fields.Float(digits=(6,2),help="Duration in days")
-    seats=fields.Integer(string="Number of seats")
-    instructor_id=fields.Many2one('res.partner','Instructor',domain=['|',('instructor','=','True'),('category_id.name','ilike','Teacher')])
-    course_id=fields.Many2one('openacademy.course',ondelete='cascade',
+    name = fields.Char(required=True)
+    start_date = fields.Date(default=fields.Date.today)
+    datetime_test = fields.Datetime(default=fields.Datetime.now)
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
+    instructor_id = fields.Many2one('res.partner', 'Instructor',
+                                    domain=['|', ('instructor', '=', 'True'),
+                                            ('category_id.name', 'ilike', 'Teacher')])
+    course_id = fields.Many2one('openacademy.course', ondelete='cascade',
                                 string="Course", required=True)
-    attendee_ids=fields.Many2many('res.partner',string="Attendees")
-    taken_seats=fields.Float(compute='_taken_seats',store=True)
-    active=fields.Boolean(default=True)
-    end_date = fields.Date(store=True,compute='_get_end_date',inverse='_set_end_date')
-    attendees_count = fields.Integer(compute='_get_attendees_count',store=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    taken_seats = fields.Float(compute='_taken_seats', store=True)
+    active = fields.Boolean(default=True)
+    end_date = fields.Date(store=True, compute='_get_end_date', inverse='_set_end_date')
+    attendees_count = fields.Integer(compute='_get_attendees_count', store=True)
     color = fields.Float()
-    hours=fields.Float(
+    hours = fields.Float(
         string="Duration in hours",
         compute='_get_hours',
         inverse='_set_hours')
@@ -82,30 +85,30 @@ class Session(models.Model):
     @api.depends('attendee_ids')
     def _get_attendees_count(self):
         for record in self:
-            record.attendees_count=len(record.attendee_ids)
+            record.attendees_count = len(record.attendee_ids)
 
-    @api.depends('start_date','duration')
+    @api.depends('start_date', 'duration')
     def _get_end_date(self):
         for record in self:
             if not (record.start_date and record.duration):
-                record.end_date =record.start_date
+                record.end_date = record.start_date
                 continue
-            
+
             start_date = fields.Datetime.from_string(record.start_date)
             # import pdb; pdb.set_trace()
-            record.end_date = start_date + timedelta(days=record.duration,seconds=-1)
-    
+            record.end_date = start_date + timedelta(days=record.duration, seconds=-1)
+
     def _set_end_date(self):
         for record in self:
-            if not (record.start_date and record.duration): 
+            if not (record.start_date and record.duration):
                 continue
-            
+
             start_date = fields.Datetime.from_string(record.start_date)
             end_date = fields.Datetime.from_string(record.end_date)
-            record.duration=(end_date-start_date).days + 1
+            record.duration = (end_date-start_date).days + 1
 
 
-    @api.depends('seats','attendee_ids')
+    @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
         #import pdb; pdb.set_trace()
         for r in self:
@@ -114,10 +117,10 @@ class Session(models.Model):
             else:
                 r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
 
-    @api.onchange('seats','attendee_ids')
+    @api.onchange('seats', 'attendee_ids')
     def _verify_valid_seats(self):
         if self.seats < 0:
-            self.active=False
+            self.active = False
             return {
                 'warning':{
                     'title': _("Incorrect 'seats' value"),
@@ -125,16 +128,16 @@ class Session(models.Model):
                 }
             }
         if self.seats < len(self.attendee_ids):
-           self.active=False
-           return {
+            self.active = False
+            return {
                 'warning':{
                     'title': _("Too many attendees"),
                     'message': _("Increase seats or remove excess attendees")
                 }
             }
-        self.active=True
-    
-    @api.constrains('instructor_id','attendee_ids')
+        self.active = True
+
+    @api.constrains('instructor_id', 'attendee_ids')
     def _check_instructor_not_in_attendees(self):
         for record in self.filtered('instructor_id'):
             if record.instructor_id in record.attendee_ids:
