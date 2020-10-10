a95c944bf313705c575fa74bda19923a8e9af662
Keyla Aviles <kaviles@alerta.com.mx>
2020-10-09 23:01:10 -0600

    [ADD] Readme file and manifest corrections

---
+++ openacademy/README.rst
@@ -0,0 +1,5 @@
+openacademy
+==============
+
+This module enable views, constraints, buttons, 
+wizard, report to manage academy
--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -3,16 +3,13 @@
     "summary": """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
-    "description": """
-        Long description of module's purpose
-    """,
-    "author": "My Company",
+    "author": "My Company, Odoo Community Association (OCA)",
     "website": "http://www.yourcompany.com",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml   # noqa
     # for the full list
     "category": "Uncategorized",
-    "version": "0.1",
+    "version": "13.0.1.0.0",
     # any module necessary for this one to work correctly
     "depends": ["base", "web", "board"],
     # always loaded
@@ -27,4 +24,5 @@
     ],
     # only loaded in demonstration mode
     "demo": ["demo/demo.xml"],
+    "license": "AGPL-3",
 }
